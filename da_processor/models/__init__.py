a4cdb60ea455edeea84bfb03ce8227317011f04c
Hariharan B <Hariharan.b@acheron-tech.com>
2025-11-29 05:20:35 +0530

    Revert ingest-monitor conversion from this repository

--- da_processor/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Data models for the DA Processor application.
-
-This package contains all data model classes used throughout the application
-for representing domain entities and facilitating data transfer between services.
-"""
-from .asset_models import IngestAsset, TitleInfo, AssetInfo
-
-__all__ = ['IngestAsset', 'TitleInfo', 'AssetInfo']
